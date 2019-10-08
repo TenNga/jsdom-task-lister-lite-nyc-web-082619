151dbeba0081c335f00a0cdd2c8b8ea072da3638
karma <kevinkarma55@gmail.com>
2019-10-08 17:42:08 -0400

    finish Deliverables, doing Stretch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let form = document.querySelector('#create-task-form');
+let list = document.querySelector('#tasks');
+let input = document.querySelector('#new-task-description');
+
+let deleteButton = document.querySelector('#delete');
+
+addList = (event) => {
+  event.preventDefault();
+  list.insertAdjacentHTML('beforeend',`<li> ${input.value}</li>`)
+}
+
+deleteList = () => {
+  list.childNodes[list.childNodes.length-1].remove();
+}
+form.addEventListener("submit", addList);
+
+deleteButton.addEventListener('click',deleteList);
